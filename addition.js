e2bb2f265898d1e966a8fab8080c83943a05f59c
DESKTOP-U8J6U5J\tamil <tamil.smartin@gmail.com>
2019-10-11 22:44:15 +0530

    adding a new file addition.js

---
+++ addition.js
@@ -0,0 +1,12 @@
+console.log("sum of two numbers");
+
+class addition {
+    constructor() {
+        this.num1 = 2;
+        this.num2 = 3;
+    }
+    sumof2Num() {
+        return this.num1 + this.num2;
+    }
+}
+export default new addition();
