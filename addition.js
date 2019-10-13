21e30176742912dd58a40a96c92fd5f465d5bf1d
DESKTOP-U8J6U5J\tamil <tamil.smartin@gmail.com>
2019-10-13 14:03:01 +0530

    adding logic with isNan

--- addition.js
+++ addition.js
@@ -6,7 +6,8 @@ class addition {
         this.num2 = 3;
     }
     sumof2Num() {
-        return this.num1 + this.num2;
+
+        return !(Number.isNaN(this.num1) || Number.isNan(this.num2)) ? this.num1 + this.num2 : '';
     }
 }
 export default new addition();
